3d93107b56419de90aaffb0fbfd28379ad85518a
atimormia <atimormia@gmail.com>
2016-10-30 22:06:34 +0300

    Task1

---
+++ Net.W.2016.01.Freydlina.05/Task1.Tests/GcdCalculatorTests.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using NUnit.Framework;
+using Task1;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    public class GcdCalculatorTests
+    {
+
+        #region Test method: public static int GcdEuclid(int a, int b)
+        public static IEnumerable<TestCaseData> TestCasesForGcdEuclid
+        {
+            get
+            {
+                yield return new TestCaseData(18, 48).Returns(6);
+                yield return new TestCaseData(18, 18).Returns(18);
+                yield return new TestCaseData(1, 0).Returns(1);
+                yield return new TestCaseData(0,1).Returns(1);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesForGcdEuclid))]
+        public int TestGcdEuclid(int a, int b)
+        {
+            Debug.WriteLine($"Euclid with {a} and {b} calculates {GcdCalculator.TimeForCalculationEuclidGcd(a,b)}");
+            return GcdCalculator.GcdEuclid(a, b);
+
+        }
+
+        public static IEnumerable<TestCaseData> TestCasesForGcdEuclidThrows
+        {
+            get
+            {
+                yield return new TestCaseData(-18, 48);
+                yield return new TestCaseData(18, -48);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesForGcdEuclidThrows))]
+        public void TestGcdEuclidThrows(int a, int b)
+        {
+            Assert.That(() => GcdCalculator.GcdEuclid(a, b), Throws.TypeOf<ArgumentException>());
+
+        }
+        #endregion
+
+        #region Test method: public static int GcdEuclid(params int[] values)
+        public static IEnumerable<TestCaseData> TestCasesForGcdEuclidParams
+        {
+            get
+            {
+                yield return new TestCaseData(new int[] { 18, 48 }).Returns(6);
+                yield return new TestCaseData(new int[] { 18, 48, 6 }).Returns(6);
+                yield return new TestCaseData(new int[] { 18, 48, 6, 1 }).Returns(1);
+                yield return new TestCaseData(new int[] { 18, 48, 6, 0, 1 }).Returns(1);
+                yield return new TestCaseData(new int[] { 0, 18, 48, 6 }).Returns(6);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesForGcdEuclidParams))]
+        public int TestGcdEuclidParams(int[] values)
+        {
+            return GcdCalculator.GcdEuclid(values);
+
+        }
+
+        public static IEnumerable<TestCaseData> TestCasesForGcdEuclidParamsThrows
+        {
+            get
+            {
+                yield return new TestCaseData(new int[] { 18, -48, 6 });
+                yield return new TestCaseData(new int[] { 18, 48, 6, 0, -1 });
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesForGcdEuclidParamsThrows))]
+        public void TestGcdEuclidParamsThrows(int[] values)
+        {
+            Assert.That(() => GcdCalculator.GcdEuclid(values), Throws.TypeOf<ArgumentException>());
+
+        }
+        #endregion
+
+        #region Test method: public static int GcdStein(int a, int b)
+        public static IEnumerable<TestCaseData> TestCasesForGcdStein
+        {
+            get
+            {
+                yield return new TestCaseData(18, 48).Returns(6);
+                yield return new TestCaseData(18, 18).Returns(18);
+                yield return new TestCaseData(1, 0).Returns(1);
+                yield return new TestCaseData(0, 1).Returns(1);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesForGcdStein))]
+        public int TestGcdStein(int a, int b)
+        {
+            Debug.WriteLine($"Stein with {a} and {b} calculates {GcdCalculator.TimeForCalculationSteinGcd(a, b)}");
+            return GcdCalculator.GcdStein(a, b);
+
+        }
+
+        public static IEnumerable<TestCaseData> TestCasesForGcdSteinThrows
+        {
+            get
+            {
+                yield return new TestCaseData(-18, 48);
+                yield return new TestCaseData(18, -48);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesForGcdSteinThrows))]
+        public void TestGcdSteinThrows(int a, int b)
+        {
+            Assert.That(() => GcdCalculator.GcdStein(a, b), Throws.TypeOf<ArgumentException>());
+
+        }
+        #endregion
+
+        #region Test method: public static int GcdStein(params int[] values)
+        public static IEnumerable<TestCaseData> TestCasesForGcdSteinParams
+        {
+            get
+            {
+                yield return new TestCaseData(new int[] { 18, 48 }).Returns(6);
+                yield return new TestCaseData(new int[] { 18, 48, 6 }).Returns(6);
+                yield return new TestCaseData(new int[] { 18, 48, 6, 1 }).Returns(1);
+                yield return new TestCaseData(new int[] { 18, 48, 6, 0, 1 }).Returns(1);
+                yield return new TestCaseData(new int[] { 0, 18, 48, 6 }).Returns(6);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesForGcdSteinParams))]
+        public int TestGcdSteinParams(int[] values)
+        {
+            return GcdCalculator.GcdStein(values);
+
+        }
+
+        public static IEnumerable<TestCaseData> TestCasesForGcdSteinParamsThrows
+        {
+            get
+            {
+                yield return new TestCaseData(new int[] { 18, -48, 6 });
+                yield return new TestCaseData(new int[] { 18, 48, 6, 0, -1 });
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesForGcdSteinParamsThrows))]
+        public void TestGcdSteinParamsThrows(int[] values)
+        {
+            Assert.That(() => GcdCalculator.GcdStein(values), Throws.TypeOf<ArgumentException>());
+
+        }
+        #endregion
+
+    }
+}
---
+++ Net.W.2016.01.Freydlina.05/Task1/GCDCalculator.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Task1
+{
+    public class GcdCalculator
+    {
+        public static int GcdEuclid(int a, int b)
+        {
+            if (a < 0 || b < 0)
+            {
+                throw new ArgumentException("The numbers must be positive");
+            }
+            if (a == 0 || a == b)
+            {
+                return b;
+            }
+            if (b == 0)
+            {
+                return a;
+            }
+            if (a == 1 || b == 1)
+            {
+                return 1;
+            }
+            if (a > b)
+            {
+                int tmp = a;
+                a = b;
+                b = tmp;
+            }
+            return GcdEuclid(a, b - a);
+        }
+
+        public static int GcdEuclid(params int[] values)
+        {
+            if (values.Length == 0)
+            {
+                throw new ArgumentException("Array is empty");
+            }
+
+            int gcd = values[0];
+            for (int i = 1; i < values.Length; i++)
+            {
+                gcd = GcdEuclid(values[i], gcd);
+                if (gcd == 1)
+                {
+                    return gcd;
+                }
+            }
+
+            return gcd;
+        }
+
+        public static TimeSpan TimeForCalculationEuclidGcd(int a, int b)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            GcdEuclid(a, b);
+            stopWatch.Stop();
+
+            return stopWatch.Elapsed;
+        }
+
+        public static int GcdStein(int a, int b)
+        {
+            if (a < 0 || b < 0)
+            {
+                throw new ArgumentException("The numbers must be positive");
+            }
+            if (a == 0 || a == b)
+            {
+                return b;
+            }
+            if (b == 0)
+            {
+                return a;
+            }
+            if (a == 1 || b == 1)
+            {
+                return 1;
+            }
+            if (a%2L == 0L && b%2L == 0L)
+            {
+                return 2*GcdStein(a/2, b/2);
+            }
+            if (a%2L == 0L && b%2L != 0L)
+            {
+                return GcdStein(a/2, b);
+            }
+            if (a%2L != 0L && b%2L == 0L)
+            {
+                return GcdStein(a, b/2);
+            }
+            if (a < b)
+            {
+                return GcdStein((b - a)/2, a);
+            }
+            return GcdStein((a - b)/2, b);
+        }
+
+        public static int GcdStein(params int[] values)
+        {
+            if (values.Length == 0)
+            {
+                throw new ArgumentException("Array is empty");
+            }
+
+            int gcd = values[0];
+            for (int i = 1; i < values.Length; i++)
+            {
+                gcd = GcdStein(values[i], gcd);
+                if (gcd == 1)
+                {
+                    return gcd;
+                }
+            }
+
+            return gcd;
+        }
+
+        public static TimeSpan TimeForCalculationSteinGcd(int a, int b)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            GcdStein(a, b);
+            stopWatch.Stop();
+
+            return stopWatch.Elapsed;
+        }
+    }
+}
